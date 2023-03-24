8d8b35c351286a2aa6263ada123b9b1169cd3c9a
taoufik <tfk.el11@gmail.com>
2023-03-23 19:45:27 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _isalpha('H');
+    _putchar(r + '0');
+    r = _isalpha('o');
+    _putchar(r + '0');
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
+julien@ubuntu:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 4-main.c 4-isalpha.c -o 4-isalpha
+julien@ubuntu:~/0x02$ ./4-isalpha 
+1110
+julien@ubuntu:~/0x02$ 
